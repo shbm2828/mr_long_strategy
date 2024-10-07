1f16f34b3a8c75b359ec8c90de32b70a20d80f35
shbm2828 <42641632+shbm2828@users.noreply.github.com>
2024-10-07 11:55:14 +0530

    Create BN_5min_candle.py
    working

---
+++ BN_5min_candle.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import time
+import schedule
+#from datetime import datetime
+#from datetime import date
+import datetime
+
+today = datetime.date.today()
+#today = datetime.date.today()
+#today = str(date.today())
+print(type(today))
+print(today)
+
+def BN_5min_candle():
+    df = pd.read_csv('D:\\key\\mr_long_data\\BN_data\\BN_live_data_'+str(today)+'.csv', header=None)
+    df.columns=['date', 'ltp']
+    df['date'] = pd.to_datetime(df.date)  
+    five_min_data = df.set_index('date').resample('5min').ohlc().reset_index()
+    five_min_data.to_csv(r'D:\\key\\mr_long_data\\BN_data\\BN_5min_candle_'+str(today)+'.csv', header=None, index=False)
+    #time.sleep(60)
+
+schedule.every(5).minutes.do(BN_5min_candle)
+while True:
+    schedule.run_pending()
+    time.sleep(5)
