181606c9f03607fc1be2024cdb338970fb18f8d1
shbm <shbm2828@gmail.com>
2024-10-15 12:59:49 +0530

    updated workflow

--- end_to_end_workflow_inte.py
+++ end_to_end_workflow_inte.py
@@ -12,9 +12,10 @@ import time
 from datetime import datetime
 import  functools
 
-from calculate_BB_RSI import EMA, RSI, bollinger_band
+from calculate_BB_RSI import RSI, bollinger_band
+from place_order import place_robo_order
 
-#from get_option_hist_data import strike_symbol_CE
+#from get_option_hist_data import strike_symbol
 
 #from calculate_BB_RSI import bollinger_band
 #from get_option_hist_data import hist_data_PE
@@ -27,7 +28,7 @@ obj = SmartConnect(api_key=key_secret[0])
 data = obj.generateSession(key_secret[2], key_secret[3], TOTP(key_secret[4]).now())
 
 instrument_file = "instrument_list.json"
-
+placed_order_id = 0
 if os.path.exists(instrument_file):
     with open(instrument_file, "r") as f:
         instrument_list = json.load(f)
@@ -42,10 +43,6 @@ else:
     print("Instrument list fetched and cached")
 
 today = dt.date.today()
-
-orderPlaced =  False
-
-
 
 def get_nearest_exp():
     today = dt.date.today()
@@ -71,21 +68,16 @@ def get_strike_price():
         strike = price + (100 - remainder)
     return strike
     
-def token_lookup_CE(strike_symbol_CE, instrument_list, exchange="NFO"):
+def token_lookup(strike_symbol, instrument_list, exchange="NFO"):
     for instrument in instrument_list:
-        if instrument["symbol"] == strike_symbol_CE :
+        if instrument["symbol"] == strike_symbol :
          return instrument["token"]
 
 
-def token_lookup_PE(strike_symbol_PE, instrument_list, exchange="NFO"):
-    for instrument in instrument_list:
-        if instrument["symbol"] == strike_symbol_PE :
-         return instrument["token"]
-
-def hist_data(token_CE,interval,st_date,end_date,instrument_list,exchange="NFO"):
+def hist_data(token,interval,st_date,end_date,instrument_list,exchange="NFO"):
     params = {
         "exchange": exchange,
-        "symboltoken": token_CE,
+        "symboltoken": token,
         "interval": interval,
         "fromdate": st_date,
         "todate": end_date,
@@ -98,162 +90,133 @@ def hist_data(token_CE,interval,st_date,end_date,instrument_list,exchange="NFO")
     df_data.index = df_data.index.tz_localize(None)
     return df_data
 
-def fetch_1_min_data_CE(token_CE, st_date_1min, end_date_1min, high, low):
-    print(
-        f"Params - token_CE: {token_CE}, st_date: {st_date_1min}, end_date: {end_date_1min}, high: {high}, low: {low}, current_minute: {datetime.now().minute}")
-    candle_df_ce_1 = hist_data(token_CE, "ONE_MINUTE", st_date_1min, end_date_1min, instrument_list)
-    close_price = candle_df_ce_1["close"].iloc[-1]
+def fetch_1_min_data_and_place_order(strike_symbol, token, st_date_1min, end_date_1min, high, low):
+    candle_df_1min = hist_data(token, "ONE_MINUTE", st_date_1min, end_date_1min, instrument_list)
+    close_price = candle_df_1min["close"].iloc[-2]
     if close_price > high:
-        print("High breached")
-        place_order(token_CE, high, low, st_date_1min, end_date_1min)
-    print(f"Close price of 1-minute candle for CE: {close_price}")
+            order_res = place_robo_order(strike_symbol, token, "BUY", 235, 15, instrument_list)
+            global placed_order_id
+            placed_order_id = order_res['data']['orderid']
 
-def fetch_1_min_data_PE(token_PE, st_date_1min, end_date_1min):
-    candle_df_pe_1 = hist_data(token_PE, "ONE_MINUTE", st_date_1min, end_date_1min, instrument_list)
-    close_price = candle_df_pe_1["close"].iloc[-1]
-    print(f"Close price of 1-minute candle for PE: {close_price}")
-
-
-def place_order(token, entry, stoploss, st_date_1min, end_date_1min):
-    global orderPlaced
-    trailStopLoss = entry + (entry - stoploss)
-    print("Trail stop loss: ", trailStopLoss)
-    bookProfit = entry + 2 * (entry - stoploss)
-    print("Book profit: ", bookProfit)
-    orderPrice = entry
-    if not orderPlaced:
-        print("Order placed")
-        orderPlaced = True
-
-        stoploss_dict = {"value": stoploss}  # Use a dictionary to store stoploss
-
-        def check_order(token, entry, stoploss_dict, st_date_1min, end_date_1min, trailStopLoss, bookProfit):
-            global orderPlaced
-            candle_df = hist_data(token, "ONE_MINUTE", st_date_1min, end_date_1min, instrument_list)
-            current_price = candle_df["close"].iloc[-1]
-            print(f"Current price: {current_price}, token: {token}, entry: {entry}, stoploss: {stoploss_dict['value']}, st_date: {st_date_1min}, end_date: {end_date_1min}, trailStopLoss: {trailStopLoss}, bookProfit: {bookProfit}")
-            if current_price <= stoploss_dict["value"]:
-                print("Stoploss hit")
-                orderPlaced = False
-                print("Stoploss hit", (current_price - orderPrice) * 100, "rupees")
-                schedule.clear('order_check')
-            elif current_price > bookProfit:
-                print("Booking profit", (current_price - orderPrice) * 100, "rupees")
-                orderPlaced = False
-                schedule.clear('order_check')  # Clear the scheduled job
-            elif current_price >= trailStopLoss:
-                print("Trailing stop loss to entry value")
-                stoploss_dict["value"] = entry  # Update the stoploss value
-
-        # Schedule the check_order function to run every 1 minute
-        schedule.every(1).minutes.do(
-            functools.partial(check_order, token, entry, stoploss_dict, st_date_1min, end_date_1min, trailStopLoss, bookProfit)
-        ).tag('order_check')
-        print("Scheduled order check")
+        
+        
+def check_open_order():
+    global placed_order_id
+    response = obj.orderBook()   #response is a dict type
+    order_book = pd.DataFrame(response["data"])
+    #strike = [strike_symbol, strike_symbol]
+    df = order_book[(order_book['status'] == 'trigger pending') & (order_book['parentorderid'] == placed_order_id) & (order_book['variety'] == 'ROBO')]
+    if df.empty:
+        return False  #currently no open order
     else:
-        print("Order already placed")
+        return True
 
-
-
-
-#strike_price = 51300
 
 
 # Function to execute every 5 minutes
 def execute_strategy():
-    global orderPlaced
-
-    strike_price = get_strike_price()
-    print("Strike Price: ", strike_price)
-
-
-    strike_symbol_CE = ('BANKNIFTY'+expiry+str(strike_price)+'CE').upper()
-    strike_symbol_PE = ('BANKNIFTY'+expiry+str(strike_price)+'PE').upper()
-
-    print(strike_symbol_CE)
-    print(strike_symbol_PE)
-
-    token_CE = token_lookup_CE(strike_symbol_CE, instrument_list)
-    token_PE = token_lookup_PE(strike_symbol_PE, instrument_list)
-    print(token_CE)
-    print(token_PE)
-
-    if today.weekday() == 0:
-        yesterday = today - dt.timedelta(days=3)
-    else:
-        yesterday = today - dt.timedelta(days=1)
-
-    st_date_5min = str(yesterday) + " 13:30"
-    end_date_5min = str(today) + " 15:30"
-
-    st_date_1min = str(today) + " 09:30"
-    end_date_1min = str(today) + " 15:30"
-
-
-    candle_df_ce_5min = hist_data(token_CE, "FIVE_MINUTE", st_date_5min, end_date_5min, instrument_list)
-    # print(candle_df)
-    candle_df_pe_5min = hist_data(token_PE, "FIVE_MINUTE", st_date_5min, end_date_5min, instrument_list)
-
-    ce_temp_bb_df = bollinger_band(candle_df_ce_5min)
-    pe_temp_bb_df = bollinger_band(candle_df_pe_5min)
-
-    
-    ce_bb = round(ce_temp_bb_df["UB"].iloc[-1], 2)
-    pe_bb = round(pe_temp_bb_df["UB"].iloc[-1], 2)
-    
-    print(ce_bb)
-    print(pe_bb)
-    
-    ce_temp_rsi_df = RSI(candle_df_ce_5min)
-    pe_temp_rsi_df = RSI(candle_df_pe_5min)
-    
-    ce_rsi = round(ce_temp_rsi_df["rsi"].iloc[-1], 2)
-    pe_rsi = round(pe_temp_rsi_df["rsi"].iloc[-1], 2)
-    print(ce_rsi)
-    print(pe_rsi)
-
-    schedule.clear('ce_job')
-
-    if ce_bb == candle_df_ce_5min["high"].iloc[-1] and ce_rsi > 60 and not orderPlaced:
-        print(strike_symbol_CE + " is a Buy")
-
-        # Get the current time
-        current_time = datetime.now()
-        current_minute = current_time.minute
-
-        # Calculate the next 5-minute boundary
-        next_5min_boundary = current_minute + (5 - (current_minute % 5))
-
-        # Define the function to be executed
-        def schedule_until_next_block():
-            current_time_inner = datetime.now()
-            current_minute_inner = current_time_inner.minute
-
-            # Stop scheduling after the next 5-minute block
-            if (current_minute_inner >= current_minute +1) and (current_minute_inner % 5 == 0):
-                print(f"Reached next 5-minute boundary: {current_time_inner.strftime('%H:%M')}, stopping the job.")
-                schedule.clear('ce_job')
-            else:
-                # Call the fetch method
-                fetch_1_min_data_CE(token_CE, st_date_1min, end_date_1min, candle_df_ce_5min["high"].iloc[-2], candle_df_ce_5min["low"].iloc[-2])
-                print(f"Data fetched at {current_time_inner.strftime('%H:%M:%S')}")
-
-        # Schedule the task every minute at 5 seconds
-        schedule.every().minute.at(":05").do(lambda: schedule_until_next_block() if not orderPlaced else None).tag('ce_job')
-    elif pe_bb == candle_df_pe_5min["high"] and pe_rsi > 60 and not orderPlaced:
-        print(strike_symbol_PE + " is a Buy")
-        for i in range(1, 5):
-            schedule.every().minute.at(f":{5 * i:02d}").do(
-                lambda: fetch_1_min_data_PE(token_PE, st_date_1min, end_date_1min)
-            )
-    else:
-        orderPlaced = False  # Ensure it's reset if no orders are placed
-        print("5min candle did not close above BB")
-
-
-# Schedule the function to run every 5 minutes
+    if check_open_order() == False:
+        #global orderPlaced
+        
+        strike_price = get_strike_price()
+        print("Strike Price: ", strike_price)
+        
+        
+        strike_symbol_CE = ('BANKNIFTY'+expiry+str(strike_price)+'CE').upper()
+        strike_symbol_PE = ('BANKNIFTY'+expiry+str(strike_price)+'PE').upper()
+        
+        print(strike_symbol_CE)
+        print(strike_symbol_PE)
+        
+        token_CE = token_lookup(strike_symbol_CE, instrument_list)
+        token_PE = token_lookup(strike_symbol_PE, instrument_list)
+        print(token_CE)
+        print(token_PE)
+        
+        if today.weekday() == 0:
+            yesterday = today - dt.timedelta(days=3)
+        else:
+            yesterday = today - dt.timedelta(days=1)
+        
+        st_date_5min = str(yesterday) + " 13:30"
+        end_date_5min = str(today) + " 15:30"
+        
+        st_date_1min = str(today) + " 09:30"
+        end_date_1min = str(today) + " 15:30"
+        
+        
+        candle_df_ce_5min = hist_data(token_CE, "FIVE_MINUTE", st_date_5min, end_date_5min, instrument_list)
+        # print(candle_df)
+        candle_df_pe_5min = hist_data(token_PE, "FIVE_MINUTE", st_date_5min, end_date_5min, instrument_list)
+        
+        ce_temp_bb_df = bollinger_band(candle_df_ce_5min)
+        pe_temp_bb_df = bollinger_band(candle_df_pe_5min)
+        
+        
+        ce_bb = round(ce_temp_bb_df["UB"].iloc[-2], 2)
+        pe_bb = round(pe_temp_bb_df["UB"].iloc[-2], 2)
+        
+        print("ce bb value: %d" %(ce_bb))
+        print("pe bb value: %d" %(pe_bb))
+        
+        ce_temp_rsi_df = RSI(candle_df_ce_5min)
+        pe_temp_rsi_df = RSI(candle_df_pe_5min)
+        
+        ce_rsi = round(ce_temp_rsi_df["rsi"].iloc[-2], 2)
+        pe_rsi = round(pe_temp_rsi_df["rsi"].iloc[-2], 2)
+        print("ce rsi value: %d" %(ce_rsi))
+        print("pe rsi value: %d" %(pe_rsi))
+        
+        schedule.clear('ce_job')
+        
+        if candle_df_ce_5min["close"].iloc[-2] > ce_bb and ce_rsi > 60:
+            # Get the current time
+            current_time = datetime.now()
+            current_minute = current_time.minute
+        
+            # Calculate the next 5-minute boundary
+            #next_5min_boundary = current_minute + (5 - (current_minute % 5))
+        
+            # Define the function to be executed
+            def schedule_until_next_block():
+                current_time_inner = datetime.now()
+                current_minute_inner = current_time_inner.minute
+        
+                # Stop scheduling after the next 5-minute block
+                if (current_minute_inner >= current_minute +1) and (current_minute_inner % 5 == 0):
+                    schedule.clear('ce_job')
+                else:
+                    fetch_1_min_data_and_place_order(strike_symbol_CE, token_CE, st_date_1min, end_date_1min, candle_df_ce_5min["high"].iloc[-2], candle_df_ce_5min["low"].iloc[-2])
+        
+        
+            # Schedule the task every minute at 5 seconds
+            schedule.every().minute.at(":05").do(lambda: schedule_until_next_block()).tag('ce_job')
+        elif candle_df_pe_5min["close"].iloc[-2] > pe_bb and pe_rsi > 60:
+            # Get the current time
+            current_time = datetime.now()
+            current_minute = current_time.minute
+        
+            # Calculate the next 5-minute boundary
+            #next_5min_boundary = current_minute + (5 - (current_minute % 5))
+        
+            # Define the function to be executed
+            def schedule_until_next_block():
+                current_time_inner = datetime.now()
+                current_minute_inner = current_time_inner.minute
+        
+                # Stop scheduling after the next 5-minute block
+                if (current_minute_inner >= current_minute +1) and (current_minute_inner % 5 == 0):
+                    schedule.clear('pe_job')
+                else:
+                    fetch_1_min_data_and_place_order(strike_symbol_PE, token_PE, st_date_1min, end_date_1min, candle_df_pe_5min["high"].iloc[-2], candle_df_pe_5min["low"].iloc[-2])
+            
+        else:
+            print("5min candle did not close above BB")
+        
+        
+        # Schedule the function to run every 5 minutes
 for minute in range(0, 60, 5):
-    schedule.every().hour.at(f":{minute:02d}").do(lambda: execute_strategy() if not orderPlaced else None)
+    schedule.every().hour.at(f":{minute:02d}").do(lambda: execute_strategy())
 
 
 while True:
--- place_order.py
+++ place_order.py
@@ -1,28 +1,5 @@
-import os
-import urllib
-from SmartApi import SmartConnect
-from pyotp import TOTP
-import json
 
-TOTP("").now()
-key_path = r"D:\key"
-os.chdir(key_path)
-key_secret = open("shbm_key.txt","r").read().split()
-obj = SmartConnect(api_key=key_secret[0])
-data = obj.generateSession(key_secret[2], key_secret[3], TOTP(key_secret[4]).now())
-
-instrument_url = "https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json"
-print("Instrument list fetched")
-response = urllib.request.urlopen(instrument_url)
-instrument_list = json.loads(response.read())
-
-
-
-
-strike_symbol_CE = "BANKNIFTY16OCT2451800CE"
-token_CE = 43745
-
-def place_robo_order(strike_symbol_CE, token_CE, buy_sell, price, quantity, instrument_list, exchange='NFO'):
+def place_robo_order(strike_symbol_CE, token_CE, buy_sell, entry_price, low, quantity, instrument_list, exchange='NFO'):
     params = {
         "variety": "ROBO",
         "tradingsymbol": strike_symbol_CE,
@@ -32,7 +9,33 @@ def place_robo_order(strike_symbol_CE, token_CE, buy_sell, price, quantity, inst
         "ordertype": "LIMIT",
         "producttype": "BO",
         "duration": "DAY",
-        "price": price,
+        "price": entry_price,
+        "stoploss": entry_price - low,         #in point
+        "squareoff": entry_price + 2*(entry_price - low),           #in point
+        "trailingstoploss": entry_price - low,         #in point
+        "quantity": quantity        
+        }
+    response = obj.placeOrder(params)
+    return response
+
+
+
+
+#strike_symbol_CE = "BANKNIFTY16OCT2451500PE"
+#token_CE = 43709
+
+
+def place_robo_order_demo(strike_symbol_CE, token_CE, buy_sell, entry_price, quantity, instrument_list, exchange='NFO'):
+    params = {
+        "variety": "ROBO",
+        "tradingsymbol": strike_symbol_CE,
+        "symboltoken": token_CE,
+        "transactiontype": buy_sell,
+        "exchange": exchange,
+        "ordertype": "LIMIT",
+        "producttype": "BO",
+        "duration": "DAY",
+        "price": entry_price,
         "stoploss": 30,         #in point
         "squareoff": 60,           #in point
         "trailingstoploss": 10,         #in point
@@ -40,8 +43,9 @@ def place_robo_order(strike_symbol_CE, token_CE, buy_sell, price, quantity, inst
         }
     response = obj.placeOrder(params)
     return response
+#order_res = place_robo_order_demo(strike_symbol_CE, token_CE, "BUY", 135, 15, instrument_list)
 
-place_robo_order(strike_symbol_CE, token_CE, "BUY", 235, 15, instrument_list)
+#placed_order_id = order_res['data']['orderid']
 
 
 
